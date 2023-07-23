92634f98a8b905e658b7a199c56269d57c126654
Marcus Lira <marcuslira@gmail.com>
2023-07-23 23:07:30 +0200

    feat: add some checks to join functions

--- utils.py
+++ utils.py
@@ -4,20 +4,21 @@ This script implements util functions for Primer Data Engineering task
 """
 LOG_TEMPLATE = "Primer Task - {0}"
 
-def nested_loop_join(dict1, dict2, key):
+def nested_loop_join(left, right, key):
     """
     Implements a simple nested loop join between two dictionaries assuming that both structures share the same key name
     """
     data = []
-    for l in dict1:
-        joined = []
-        for r in dict2:
-            if l[key] == r[key]:
-                l_copy = l.copy()
-                l_copy.update(r)
-                joined.append(l_copy)
-        if len(joined) > 0:
-            data = data + joined
+    if left and right and key:
+        for l in left:
+            joined = []
+            for r in right:
+                if l[key] == r[key]:
+                    l_copy = l.copy()
+                    l_copy.update(r)
+                    joined.append(l_copy)
+            if len(joined) > 0:
+                data = data + joined
     return data
 
 
@@ -25,44 +26,46 @@ def merge_join(left, right, key):
     """
     Implements a sort-merge join between two dictionaries assuming that both structures share the same key name
     """
-    inner = sorted(left, key=lambda d: d[key])
-    outer = sorted(right, key=lambda d: d[key])
+    if left and right and key:
+        inner = sorted(left, key=lambda d: d[key])
+        outer = sorted(right, key=lambda d: d[key])
 
-    i = 0
-    o = 0
+        i = 0
+        o = 0
 
-    while i < len(inner) and o < len(outer):
-        k = min(inner[i][key], outer[o][key])
+        while i < len(inner) and o < len(outer):
+            k = min(inner[i][key], outer[o][key])
 
-        inner_group = []
-        outer_group = []
+            inner_group = []
+            outer_group = []
 
-        # Addesses cases where there are multiple rows on the left and right for the same key
-        while (i < len(inner) and k == inner[i][key]):
-            inner_group.append(inner[i])
-            i += 1
+            # Addesses cases where there are multiple rows on the left and right for the same key
+            while (i < len(inner) and k == inner[i][key]):
+                inner_group.append(inner[i])
+                i += 1
 
-        while (o < len(outer) and k == outer[o][key]):
-            outer_group.append(outer[o])
-            o += 1
+            while (o < len(outer) and k == outer[o][key]):
+                outer_group.append(outer[o])
+                o += 1
 
-        for ig in inner_group:
-            for og in outer_group:
-                yield ig | og
+            for ig in inner_group:
+                for og in outer_group:
+                    yield ig | og
 
 def hash_join(left, right, key):
     """
     Implements a hash join between two dictionaries assuming that both structures share the same key name
     """
-    hash_table = {}
-    res = []
-    for v in right:
-        hash_table[v[key]] = hash_table.get(v[key], []) + [v]
+    if left and right and key:
+        hash_table = {}
+        res = []
+        for v in right:
+            hash_table[v[key]] = hash_table.get(v[key], []) + [v]
 
-    for l in left:
-        if l[key] in hash_table:
-            for v in hash_table[l[key]]:
-                yield l | v 
+        for l in left:
+            if l[key] in hash_table:
+                for v in hash_table[l[key]]:
+                    yield l | v 
 
 
 def flatten_dict(d):
